672b0857e9d606ac8b2a05e2b9d090bb83340312
Oscar Lewis <o.lewis11@gmail.com>
2018-04-22 14:26:26 -0700

    add lab exercises

--- week-2/exercise.R
+++ week-2/exercise.R
@@ -1,7 +1,7 @@
 ## Part 1: Debugging
 
 my_num <- 6
-initials <- "?. ?."
+initials <- "O. L."
 
 my_vector <- c(my_num, initials)
 
@@ -9,40 +9,56 @@ my_vector <- c(my_num, initials)
 vector_sum <- sum(my_vector)
 
 # Describe why this doesn't work: 
-# 
+# Can not add 6 and "O. L." together because 6 is a number and "O. L." is a string
 
 install.packages("stringr")
+
 
 my_line <- "Hey, hey, this is the library"
 
 print(str_length(my_line))
 
 # Describe why this doesn't work: 
-# 
+# Library () hasn't been used to load the stringr package
 
 said_the_famous <- paste(my_line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# initial is not a stored variable
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
+my_vector <- c(10, 9, 2016)
+typeof(my_vector)
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(vector_1, vector_2) {
+  diff <- abs(length(vector_1) - length(vector_2))
+  result <- paste("The difference in lengths is ", diff)
+  return(result)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+CompareLength(c(1,2,3), 1:100)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(v1, v2) {
+  diff <- length(v1) - length(v2)
+  if (diff > 0) {
+    result <- paste("Your first vector is longer by ", diff, "elements") 
+  } else {
+    result <- paste("Your second vector is longer by ", diff, "elements")
+  }
+  
+  return(result)
+}
 # Pass two vectors to your `DescribeDifference` function
-
+DescribeDifference(c(1,2,3,4), c(1,2,3,4,5))
 
 # Write a function `CombineVectors` that takes in 3 vectors and combines them into one
 
--- week-3/exercise.R
+++ week-3/exercise.R
@@ -1,10 +1,13 @@
 iris_df <- data.frame(iris)
 
-# Select all values of data frame for Sepal.length
+# Select all values of data framefor Sepal.length
+iris_df$Sepal.Length
 
 # Select only rows of Virginica flowers
+iris_df[iris_df$Species == "virginica", ]
 
 # Select only the sepal legnth of the Virginica flowers
+iris_df[iris_df$Species == "virginica", "Sepal.Length"]
 
 # Select rows where Petal.Length > 4.0
 
